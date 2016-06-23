352a56c42beddbbd99d68e779857928637bd466f
Raphaël M-P <rmarquispelletier@gmail.com>
2016-06-23 12:57:56 -0400

    Works for all numbers

--- roman.py
+++ roman.py
@@ -20,76 +20,25 @@ def convertToRoman(arabic):
             roman += numeral
             arabic = arabic - value
 
-
     roman = roman.replace('VIIII','IX')
     roman = roman.replace('IIII','IV')
     roman = roman.replace('LXXXX', 'XC')
     roman = roman.replace('XXXX', 'XL')
+    roman = roman.replace('DCCCC', 'CM')
+    roman = roman.replace('CCCC', 'CD')
 
-#    roman = roman.replace('IVI','V')
     return roman
 
 def testConvertToRoman():
-    testCases = {
-    1: 'I',
-    2: 'II',
-    3: 'III',
-    4: 'IV',
-    5: 'V',
-    6: 'VI',
-    7: 'VII',
-    8: 'VIII',
-    9: 'IX',
-    10: 'X',
-    11: 'XI',
-    12: 'XII',
-    13: 'XIII',
-    14: 'XIV',
-    15: 'XV',
-    16: 'XVI',
-    17: 'XVII',
-    18: 'XVIII',
-    19: 'XIX',
-    20: 'XX',
-    21: 'XXI',
-    22: 'XXII',
-    23: 'XXIII',
-    24: 'XXIV',
-    38: 'XXXVIII',
-    39: 'XXXIX',
-    40: 'XL',
-    41: 'XLI',
-    47: 'XLVII',
-    49: 'XLIX',
-    50: 'L',
-    51: 'LI',
-    52: 'LII',
-    59: 'LIX',
-    60: 'LX',
-    61: 'LXI',
-    80: 'LXXX',
-    81: 'LXXXI',
-    83: 'LXXXIII',
-    89: 'LXXXIX',
-    90: 'XC',
-    91: 'XCI',
-    92: 'XCII',
-    95: 'XCV',
-    97: 'XCVII',
-    98: 'XCVIII',
-    99: 'XCIX',
-    100: 'C',
-    101: 'CI',
-    157: 'CLVII',
-    179: 'CLXXIX',
+    arabic = 0
+    with open('cases.txt') as f:
+        for line in f:
+            line = line.rstrip('\n')
+            arabic += 1
+            if convertToRoman(arabic) != line:
+                raise Exception('convertToRoman(%d) gave %s, expected %s' % (arabic, convertToRoman(arabic), line))
 
-    498: 'CDXCVIII'
-    }
-
-    for arabic, roman in testCases.iteritems():
-        if convertToRoman(arabic) != roman:
-            raise Exception('convertToRoman(%d) gave %s, expected %s' % (arabic, convertToRoman(arabic), roman))
-    print('All tests passed.')
+        print('All tests passed.')
 
 if argv[1] == '--test':
     testConvertToRoman()
